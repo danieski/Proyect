3cd707cd04d5f59cbdf9e48bc3299d1d0f79f6e8
danieski <danielesquivelgarcia@gmail.com>
2022-05-16 13:05:55 +0100

    Change Feed 8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="/css/main.css" rel="stylesheet">
-<div class="social-feed-box">
+<div class="social-feed-box w-50 p-3">
 
     <div class="pull-right social-action dropdown">
        
