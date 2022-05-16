23b2558b553d9813736b0feddd9d02b1245558ed
danieski <danielesquivelgarcia@gmail.com>
2022-05-16 11:54:28 +0100

    Change Feed 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,9 @@
 @else
     
 @endif
-<table>
+
 @foreach ($Posts as $Post )
-<tr>
- <td>{{$Post->body}}</td>
- <td>{{$Post->created_at}}</td>
-</tr>
-  
-@endforeach
-</table>
+
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="/css/main.css" rel="stylesheet">
 <div class="social-feed-box">
@@ -40,15 +34,12 @@
             <a href="#">
                 Andrew Williams
             </a>
-            <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
+            <small class="text-muted">{{$Post->created_at}}</small>
         </div>
     </div>
     <div class="social-body">
         <p>
-            Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-            default model text, and a search for 'lorem ipsum' will uncover many web sites still
-            in their infancy. Packages and web page editors now use Lorem Ipsum as their
-            default model text.
+            {{$Post->body}}
         </p>
 
         <div class="btn-group">
@@ -57,19 +48,5 @@
             <button class="btn btn-white btn-xs"><i class="fa fa-share"></i> Share</button>
         </div>
     </div>
-    <div class="social-footer">
-        <div class="social-comment">
-            <a href="" class="pull-left">
-                <img alt="image" src="https://bootdey.com/img/Content/avatar/avatar1.png">
-            </a>
-            <div class="media-body">
-                <a href="#">
-                    Andrew Williams
-                </a>
-                Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words.
-                <br>
-                <a href="#" class="small"><i class="fa fa-thumbs-up"></i> 26 Like this!</a> -
-                <small class="text-muted">12.06.2014</small>
-            </div>
-        </div>
+@endforeach
 @endsection
