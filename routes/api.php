18fd726c321ff789984e16074160d3625bad817a
danieski <danielesquivelgarcia@gmail.com>
2022-05-17 16:35:09 +0100

    AAA

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use App\Http\Controllers\ApiController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register', [ApiController::class. 'register']);
+Route::post('/register', [ApiController::class, 'register']);
