c74c4392e9bb5fa916db69cfde993c1db83fe4e9
danieski <danielesquivelgarcia@gmail.com>
2022-05-17 16:08:36 +0100

    Last commit of the day

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Ilumninate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {
-    //
+    public function login (Request $request){
+        $login = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        if( !Auth::attempt( $login )) {
+            return response([ 'message' => 'Invalid credecials']);
+        }
+        $accessToken = Auth::user()->createToken('authToken')->accessToken;
+        
+        return response(['user' => Auth::user(), 'access_token' => $accessToken]);
+    }
 }
