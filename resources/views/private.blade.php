a37ea15c75acc8f9ae0f4f9cea982e6490e358a0
danieski <danielesquivelgarcia@gmail.com>
2022-05-18 12:35:36 +0100

    3 day

--- resources/views/private.blade.php
+++ resources/views/private.blade.php
@@ -39,7 +39,7 @@ td{
 <section class="vh-100 gradient-custom">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center h-75">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+
         <div class="card bg-dark text-white" style="border-radius: 1rem;">
           <div class="card-body p-5 text-center">
     <div class="card-header">
