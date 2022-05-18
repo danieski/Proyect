4ce513de61211655c76da07ce00c01abefc35685
danieski <danielesquivelgarcia@gmail.com>
2022-05-18 12:24:39 +0100

    3 day

--- resources/views/private.blade.php
+++ resources/views/private.blade.php
@@ -86,12 +86,13 @@ td{
 
       
   </div>
+  <a class="btn btn-primary btn-lg btn-block" href={{"delete/".auth()->user()->id }}>Delete</a>
+<a class="btn btn-primary btn-lg btn-block" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
   @endif
 </div>
 </div>
 </div>
-<a class="btn btn-primary btn-lg btn-block" href={{"delete/".auth()->user()->id }}>Delete</a>
-<a class="btn btn-primary btn-lg btn-block" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+
 </div>
 </div>
 
