77dd88c399ba4edabf95fdab03a94e217f0fcd83
danieski <danielesquivelgarcia@gmail.com>
2022-05-18 11:39:22 +0100

    3 day

--- resources/views/private.blade.php
+++ resources/views/private.blade.php
@@ -50,7 +50,7 @@ td{
 <section class="vh-100 gradient-custom">
   <div class="container py-1 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+      <div class="col-12 col-md-4 col-lg-6 col-xl-5">
         <div class="card bg-dark text-white" style="border-radius: 1rem;">
           <div class="card-body p-5 text-center">
               <div class="mb-md-5 mt-md-5 pb-5">
