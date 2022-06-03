32d1b1479a19398e2b5fa1114393b9f36aaffcbc
danieski <danielesquivelgarcia@gmail.com>
2022-06-03 14:38:44 +0100

    Payment

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -1,4 +1,12 @@
 @extends("layouts.app")
 @section("content")
 <h1>Subscription</h1>
+<input id="card-holder-name" type="text">
+ 
+<!-- Stripe Elements Placeholder -->
+<div id="card-element"></div>
+ 
+<button id="card-button">
+    Process Payment
+</button>
 @endsection
